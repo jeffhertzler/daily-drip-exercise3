cc3d7d97734d4bbca8e71ff539d9ca71766d8ef9
Jeff Hertzler <jeffhertzler@gmail.com>
2017-03-21 22:03:58 -0400

    reimplement RPN as a GenServer

---
+++ lib/rpn.ex
@@ -0,0 +1,38 @@
+defmodule Rpn do
+  use GenServer
+
+  ### Client API
+  def start do
+    GenServer.start(__MODULE__, [], [])
+  end
+
+  def peek(pid) do
+    GenServer.call(pid, :peek)
+  end
+
+  def push(pid, item) do
+    GenServer.cast(pid, {:push, item})
+  end
+
+  ### Server API
+  def init(_) do
+    {:ok, []}
+  end
+
+  def handle_call(:peek, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:push, :+}, [second | [first | tail]]) do
+    {:noreply, [first + second | tail]}
+  end
+  def handle_cast({:push, :-}, [second | [first | tail]]) do
+    {:noreply, [first - second | tail]}
+  end
+  def handle_cast({:push, :x}, [second | [first | tail]]) do
+    {:noreply, [first * second | tail]}
+  end
+  def handle_cast({:push, item}, state) do
+    {:noreply, [item | state]}
+  end
+end
